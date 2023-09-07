bba2eb04cab8f5d411986b10d386714f5a3e82ed
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-09-07 09:40:02 +0100

    :lipstick: Added flex for basic positioning

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <nav>
-      <section>
+      <section className='flex'>
         {/* Logo */}
         <div className='logo-background flex h-7 w-7 items-center justify-center rounded-full md:h-9 md:w-9'>
           <p className='font-semibold text-white900 md:text-xl'>J</p>
