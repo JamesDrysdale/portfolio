42eba40e700f2daa562f9d6a8e3663bce093baa9
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-09-07 09:37:56 +0100

    Mobile menu button shows up conditionally

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,36 @@
 'use client';
 
+import { useState } from 'react';
+import Image from 'next/image';
+
+import {
+  menuLight,
+  menuDark,
+  downloadLight,
+  downloadDark,
+  themeLight,
+  themeDark,
+} from '@/public/icons/navbar-icons';
+
 const Navbar = () => {
+  const [showMobileNavbar, setShowMobileNavbar] = useState(false);
+
   return (
     <nav>
-      {/* Logo */}
-      <div className='logo-background flex h-7 w-7 items-center justify-center rounded-full md:h-9 md:w-9'>
-        <p className='font-semibold text-white900 md:text-xl'>J</p>
-      </div>
+      <section>
+        {/* Logo */}
+        <div className='logo-background flex h-7 w-7 items-center justify-center rounded-full md:h-9 md:w-9'>
+          <p className='font-semibold text-white900 md:text-xl'>J</p>
+        </div>
+        <Image
+          src={menuLight}
+          height={24}
+          width={24}
+          alt='menu button'
+          className='cursor-pointer md:hidden'
+          onClick={() => setShowMobileNavbar((prev) => !prev)}
+        />
+      </section>
     </nav>
   );
 };
