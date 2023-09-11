3056fd962a2689c99db2c6d90a6e20dea1b746e2
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-09-11 03:33:57 +0100

    Add necessary assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero';
+import Skills from '@/components/homepage/Skills';
 
 export default function Home() {
   return (
     <main className='mt-24'>
       <Hero />
+      <Skills />
     </main>
   );
 }
---
+++ components/homepage/Skills.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Skills = () => {
+  return (
+    <section className='flex justify-center'>
+      <h2 className='text-underline inline-block'>Skills</h2>
+    </section>
+  );
+};
+
+export default Skills;
