516577cf1fcc0cd8d3c99dca8e15d5a9428fbc3c
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-09-11 01:27:47 +0100

    Hero image is fully responsive and looks good across all screen sizes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
+
+import { heroImg } from '@/public/images';
+
 const Hero = () => {
   return (
-    <section className='mb-7 md:flex'>
+    <section className='mx-auto mt-16 flex w-full max-w-7xl flex-col md:mt-24  md:flex-row md:justify-between  md:px-10'>
       {/* Intro */}
-      <div>
-        <p className='text-sm font-semibold uppercase leading-tight tracking-widest text-primaryLight dark:text-primaryDark md:text-xl md:leading-relaxed'>
+      <div className='z-20 flex flex-col md:w-[38rem]'>
+        <p className='text-small mb-3 font-semibold uppercase tracking-widest text-primaryLight dark:text-primaryDark md:text-xl'>
           Hi, I am James
         </p>
         <h1>
@@ -11,28 +15,34 @@ const Hero = () => {
           <span className='text-underline'>Web Developer</span> <br />
           based in UK
         </h1>
-        <p className='text-body text-white500 dark:text-white800'>
-          Transforming the web one line of code at a time: <br />
-          Crafting cutting-edge digital experiences with precision, <br />
-          passion, and a profound commitment to excellence.
+        <p className='text-hero mt-3 text-white500 dark:text-white800'>
+          Transforming the web one line of code at a time: Crafting cutting-edge
+          digital experiences with precision, passion and a profound commitment
+          to excellence.
         </p>
-        <div className='mt-9 flex flex-col gap-2 md:flex-row'>
+        <div className='my-3 flex flex-col justify-start gap-2 md:flex-row'>
           <a
             href='/'
-            className='inline-flex h-12 items-center justify-center rounded-full bg-primaryLight py-3 text-sm font-semibold text-white900 dark:bg-primaryDark md:w-48 md:px-3 md:py-5 md:text-lg'
+            className='inline-flex h-9 items-center justify-center rounded-full bg-primaryLight py-3 text-xs font-semibold text-white900 dark:bg-primaryDark md:w-32 md:px-3 lg:py-5 lg:text-sm xl:w-48'
           >
             My Work
           </a>
           <a
             href='/'
-            className='inline-flex h-12 items-center justify-center rounded-full bg-white900 py-4 font-semibold text-white500 dark:bg-black200 dark:text-white900 md:w-52 md:py-6'
+            className='inline-flex h-9 items-center justify-center rounded-full bg-white500 py-3 text-xs font-semibold text-white900 dark:bg-primaryDark md:w-32 md:px-3 lg:py-5 lg:text-sm xl:w-48'
           >
             Contact Me
           </a>
         </div>
       </div>
-      {/* Hero Image */}
-      <div className='md:w-[50%]'>Image goes here</div>
+      {/* Image */}
+      <div className='self-center'>
+        <Image
+          src={heroImg}
+          alt='hero image icon'
+          className='md:max-h-[500px] md:max-w-[500px]'
+        />
+      </div>
     </section>
   );
 };
